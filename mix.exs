ad5b533264cfa85a1847bd1126cd69cb215f4442
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-10-20 23:45:12 +0200

    Bump vsn

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Decorator.Mixfile do
 
   def project do
     [app: :decorator,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
