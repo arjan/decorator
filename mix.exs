dcead0f5d907ca7fbbff0a0d87d5408010a68177
Arjan Scherpenisse <arjan@scherpenisse.net>
2017-01-24 14:31:18 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Decorator.Mixfile do
 
   def project do
     [app: :decorator,
-     version: "1.1.0",
+     version: "1.2.0",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
