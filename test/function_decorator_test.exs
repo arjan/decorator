e201b2182dd7ceb209ea58ada619a7e6b334e90d
Ludwik Bukowski <ludwik.bukowski@gmail.com>
2018-10-01 11:39:38 +0200

    Add test for empty body

--- test/function_decorator_test.exs
+++ test/function_decorator_test.exs
@@ -63,6 +63,16 @@ defmodule DecoratorTest.Fixture.PrivateDecorated do
   defp foo(x), do: x
 end
 
+defmodule DecoratorTest.Fixture.DecoratedFunctionWithEmptyClause do
+  use DecoratorTest.Fixture.FunctionResultDecorator
+
+   @decorate function_result(:ok)
+  def multiply(x, y \\ 1)
+
+  def multiply(1, y) do y end
+  def multiply(x, y) do x * y end
+ end
+
 # Tests itself
 defmodule DecoratorTest.FunctionDecorator do
   use ExUnit.Case
@@ -71,6 +81,7 @@ defmodule DecoratorTest.FunctionDecorator do
     MyFunctionResultModule,
     DecoratedFunctionClauses,
     DecoratedFunctionWithDifferentArities,
+    DecoratedFunctionWithEmptyClause,
     FunctionResultDecorator,
     PrivateDecorated
   }
@@ -89,6 +100,12 @@ defmodule DecoratorTest.FunctionDecorator do
   test "decorating a function with different arity heads" do
     assert {:ok, 3} == DecoratedFunctionWithDifferentArities.testfun(1, 2)
     assert {:ok, 5} == DecoratedFunctionWithDifferentArities.testfun(5)
+  end
+
+ test "decorating a function with an empty clause" do
+    assert {:ok, 11} == DecoratedFunctionWithEmptyClause.multiply(11)
+    assert {:ok, 24} == DecoratedFunctionWithEmptyClause.multiply(6,4)
+    assert {:ok, 5} == DecoratedFunctionWithEmptyClause.multiply(1,5)
   end
 
   test "should throw error when defining an unknown decorator" do
