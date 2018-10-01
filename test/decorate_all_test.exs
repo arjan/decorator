e0eeff92e4d6670958434f5922b28641a530026a
Ludwik Bukowski <ludwik.bukowski@gmail.com>
2018-10-01 13:26:41 +0200

    Test for decorate_all with custom attribute

--- test/decorate_all_test.exs
+++ test/decorate_all_test.exs
@@ -26,11 +26,28 @@ defmodule DecoratorDecorateAllTest.Fixture.MyModule do
 
   def empty_body(10), do: 11
   def empty_body(n), do: n+2
+
+end
+
+defmodule DecoratorDecorateAllTest.Fixture.MyModuleWithAttribute do
+  use DecoratorDecorateAllTest.Fixture.MyDecorator
+
+  @decorate_all some_decorator()
+
+  @custom_attr 15
+  @custom_attr_map %{some_val: 3, other_val: 10}
+
+  def fun1(x), do: x+2
+
+  def fun2(x), do: x + @custom_attr
+
+  def fun3(x), do: x + @custom_attr_map[:some_val]
 end
 
 defmodule DecoratorDecorateAllTest do
   use ExUnit.Case
   alias DecoratorDecorateAllTest.Fixture.MyModule
+  alias DecoratorDecorateAllTest.Fixture.MyModuleWithAttribute
 
   test "decorate_all" do
     assert {:ok, 4} == MyModule.square(2)
@@ -40,5 +57,8 @@ defmodule DecoratorDecorateAllTest do
     assert {:ok, 666} == MyModule.value666()
     assert {:ok, 11} == MyModule.empty_body(10)
     assert {:ok, 8} == MyModule.empty_body(6)
+    assert {:ok, 10} == MyModuleWithAttribute.fun1(8)
+    assert {:ok, 20} == MyModuleWithAttribute.fun2(5)
+    assert {:ok, 8} == MyModuleWithAttribute.fun3(5)
   end
 end
