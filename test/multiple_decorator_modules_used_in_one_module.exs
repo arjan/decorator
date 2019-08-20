ac35c1c635dc09e0d684b89d769b06f12b8496d3
ryanbjones <run0391@gmail.com>
2019-08-20 13:59:12 -0600

    Add an attribute to module to check if it has already been decorated
    Prevent separate decorators from calling on_definition each time, which causes duplicate function definitions and throws warnings of previous definitions. This problem also prevents two separate decorator modules from being included if any function in the module has a default value

---
+++ test/multiple_decorator_modules_used_in_one_module.exs
@@ -0,0 +1,36 @@
+defmodule DecoratorTest.Fixture.MonitoringDecorator do
+  use Decorator.Define, some_decorator: 0
+
+  def some_decorator(body, _context) do
+    {:ok, body}
+  end
+end
+
+defmodule DecoratorTest.Fixture.LoggingDecorator do
+  use Decorator.Define, test_log: 0
+
+  def test_log(body, _context) do
+    {:ok, body}
+  end
+end
+
+
+defmodule DecoratorTest.Fixture.TwoDecoratorsUsed do
+  use DecoratorTest.Fixture.LoggingDecorator
+  use DecoratorTest.Fixture.MonitoringDecorator
+
+  @decorate some_decorator()
+  def result(default \\ nil) do
+    default
+  end
+end
+
+defmodule DecoratorTest.MultipleDecoratorModules do
+  use ExUnit.Case
+  alias DecoratorTest.Fixture.TwoDecoratorsUsed
+
+  test "module compiles and is not redefined" do
+    assert {:ok, "tested"} == TwoDecoratorsUsed.result("tested")
+    assert {:ok, nil} == TwoDecoratorsUsed.result()
+  end
+end
